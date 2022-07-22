28a710b83fdaaa2db52c11ec5f24c53053c5859c
Daniel Tak <danieljtak@hotmail.com>
2022-07-22 17:13:14 +0000

    issue 2 in progress - anchor and viewswapping needed

--- js/main.js
+++ js/main.js
@@ -26,3 +26,47 @@ function handleSubmit(event) {
 }
 
 journalEntry.addEventListener('submit', handleSubmit);
+
+function renderJournal(entry) {
+  var list = document.createElement('li');
+  list.setAttribute('class', 'li');
+
+  var row = document.createElement('div');
+  row.setAttribute('class', 'row');
+
+  var columnHalf = document.createElement('div');
+  columnHalf.setAttribute('class', 'column-half');
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photoUrl);
+
+  var columnHalf2 = document.createElement('div');
+  columnHalf2.setAttribute('class', 'column-half');
+
+  var title = document.createElement('h3');
+  title.textContent = entry.title;
+
+  var notesText = document.createElement('p');
+  notesText.textContent = entry.notes;
+
+  ul.appendChild(list);
+  list.appendChild(row);
+  row.appendChild(columnHalf);
+  columnHalf.appendChild(image);
+  row.appendChild(columnHalf2);
+  columnHalf2.appendChild(title);
+  columnHalf2.appendChild(notesText);
+
+  return list;
+
+}
+
+var ul = document.querySelector('ul');
+
+function handleEventDom(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var newEntry = renderJournal(data.entries[i]);
+    ul.append(newEntry);
+  }
+}
+window.addEventListener('DOMContentLoaded', handleEventDom);
