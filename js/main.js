f747483990990d045cbc13b1ef5dc73a63b1e131
Daniel Tak <danieljtak@hotmail.com>
2022-07-29 22:41:23 +0000

    Completed issue-4 first pass

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var photoUrl = document.querySelector('.photo-url');
+var photoUrl = document.querySelector('#photo-url');
 var image = document.querySelector('img');
 
 function handleEvent(event) {
@@ -19,14 +19,26 @@ function handleSubmit(event) {
     entryId: data.nextEntryId
   };
 
-  data.nextEntryId++;
   journalEntry.reset();
   image.setAttribute('src', 'images/placeholder-image-square.jpg');
-  data.entries.unshift(completedForm);
-  var newEntry = renderJournal(completedForm);
-  ul.prepend(newEntry);
   viewSwap('entries');
 
+  var $entry = renderJournal(completedForm);
+  if (data.editing !== null) {
+    completedForm.entryId = data.editing.entryId;
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.editing.entryId === data.entries[i].entryId) {
+        data.entries[i] = completedForm;
+        ul.children[i].replaceWith($entry);
+        data.editing = null;
+        break;
+      }
+    }
+  } else {
+    data.nextEntryId++;
+    data.entries.unshift(completedForm);
+    ul.prepend($entry);
+  }
 }
 
 journalEntry.addEventListener('submit', handleSubmit);
@@ -44,8 +56,8 @@ function renderJournal(entry) {
   var image = document.createElement('img');
   image.setAttribute('src', entry.photoUrl);
 
-  var columnHalf2 = document.createElement('div');
-  columnHalf2.setAttribute('class', 'column-half');
+  var columnRight = document.createElement('div');
+  columnRight.setAttribute('class', 'column-half');
 
   var title = document.createElement('h3');
   title.textContent = entry.title;
@@ -53,13 +65,20 @@ function renderJournal(entry) {
   var notesText = document.createElement('p');
   notesText.textContent = entry.notes;
 
+  var editPen = document.createElement('i');
+  editPen.className = 'fa-regular fa-pen-to-square icon';
+
+  // Ensure that each rendered entry is given a data-entry-id attribute indicating which entry it is.
+  list.setAttribute('data-entry-id', entry.entryId);
+
   ul.appendChild(list);
   list.appendChild(row);
   row.appendChild(columnHalf);
   columnHalf.appendChild(image);
-  row.appendChild(columnHalf2);
-  columnHalf2.appendChild(title);
-  columnHalf2.appendChild(notesText);
+  row.appendChild(columnRight);
+  columnRight.appendChild(title);
+  columnRight.appendChild(notesText);
+  title.appendChild(editPen);
 
   return list;
 
@@ -96,5 +115,57 @@ function viewSwap(viewEntry) {
 }
 
 for (var i = 0; i < button.length; i++) {
+
   button[i].addEventListener('click', currentWindow);
 }
+
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+
+function editButton(event) {
+  if (event.target.matches('.icon')) {
+    viewSwap('new-entry');
+  }
+  if (event.target && event.target.tagName === 'I') {
+    var $closest = event.target.closest('.li');
+    var entryId = $closest.getAttribute('data-entry-id');
+
+    for (var g = 0; g < data.entries.length; g++) {
+      if (Number(entryId) === data.entries[g].entryId) {
+        data.editing = data.entries[g];
+      }
+      $title.value = data.editing.title;
+      photoUrl.value = data.editing.photoUrl;
+      $notes.value = data.editing.notes;
+      image.setAttribute('src', data.editing.photoUrl);
+    }
+  }
+}
+
+ul.addEventListener('click', editButton);
+var $modal = document.querySelector('.modal');
+var $delete = document.querySelector('.delete');
+var $cancel = document.querySelector('#cancel');
+var $confirm = document.querySelector('#confirm');
+
+function openModal(event) {
+  $modal.className = 'modal';
+}
+function closeModal(event) {
+  $modal.className = 'modal hidden';
+}
+
+function aDelete(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    if (data.entries[i].entryId === data.editing.entryId) {
+      data.entries.splice(i, 1);
+      ul.children[i].remove();
+      viewSwap('entries');
+      closeModal();
+    }
+  }
+}
+
+$delete.addEventListener('click', openModal);
+$cancel.addEventListener('click', closeModal);
+$confirm.addEventListener('click', aDelete);
